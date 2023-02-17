57042b5e92edb5dd901d3c2cf179bff57465e8ac
John Leider <john@vuetifyjs.com>
2023-02-17 17:30:44 -0600

    chore: save test commit

--- packages/vuetify/src/adapters/date-fns.ts
+++ packages/vuetify/src/adapters/date-fns.ts
@@ -8,4 +8,40 @@ export default class DateFnsAdapter implements DateAdapter<Date> {
   getWeek (date: Date) {
     return getWeek(date)
   }
+
+  addDays (date: Date, amount: number) {
+    return null
+  }
+
+  date () {
+    return null
+  }
+
+  format (date: Date, formatString: string) {
+    return null
+  }
+
+  getMonth (date: Date) {
+    return null
+  }
+
+  getWeekArray (month: Date) {
+    return null
+  }
+
+  getYear (date: Date) {
+    return null
+  }
+
+  isSameDay (date: Date, comparing: Date) {
+    return null
+  }
+
+  isSameMonth (date: Date, comparing: Date) {
+    return null
+  }
+
+  isWithinRange (date: Date, range: [Date, Date]) {
+    return null
+  }
 }
